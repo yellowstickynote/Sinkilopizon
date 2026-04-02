73dbd48ce57bf2e9961734d9c043ca2109357fc9
wassim <wassim_fkih_pr@proton.me>
2026-04-02 13:28:52 +0100

    Fix test.sh

--- test.sh
+++ test.sh
@@ -7,24 +7,27 @@ g++ brt.cpp -o stress/brt
 g++ sol.cpp -o stress/sol
 g++ verif.cpp -o stress/verif
 cd stress
-for i in {1..500}; do
-	input=`./gen`
-	brtout=$(./brt <<< "$input")
-	solout=$(./sol <<< "$input")
-	exit_code=$?
+for i in {1..50000}; do
+    input=`./gen`
+    brtout=$(./brt <<< "$input")
+    solout=$(./sol <<< "$input")
+    exit_code=$?
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    NC='\033[0m' # No Color
     if [ $exit_code -ne 0 ]; then
-		echo -e "\e[31mexited with code $exit_code on test $i\e"
+        echo -e "${RED}runtime error on test $i ${NC}"
+        printf "fuck"
         echo "$input" > failed.in
         break
     fi
-	echo "$input $brtout $solout"
-	ver=$(./verif <<< "$input $brtout $solout")
-	if [ "$ver" != "0" ]; then
-		echo -e "\e[31mfailed on test $i\e"
-		echo -e "$input" > failed.in
-		echo -e "$brtout" > correct.txt
-		echo -e "$solout" > wrong.txt
-		break
-	fi
-	echo -e "\e[32maccepted on test $i\e"
+    ver=$(./verif <<< "$input $brtout $solout")
+    if [ "$ver" != "1" ]; then
+        echo -e "${RED}wrong answer on test $i ${NC}"
+        echo -e "$input" > failed.txt
+        echo -e "$brtout" > correct.txt
+        echo -e "$solout" > wrong.txt
+        break
+    fi
+    echo -e "${GREEN}accepted on test $i ${NC}"
 done

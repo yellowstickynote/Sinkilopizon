2b1409abbd63b6aa056816c582c555ebc4780c45
wassim <wassim_fkih_pr@proton.me>
2026-05-28 19:14:44 +0100

    Rewrite lazy_propagation.cpp into iterative version

--- Data_Structures/lazy_propagation.cpp
+++ Data_Structures/lazy_propagation.cpp
@@ -1,65 +1,85 @@
 /**
- * Lazy Segment Tree (Range Update, Range Query)
- * Interchangeable with standard SegTree. 
- * Default configured for Range Add / Range Sum.
+ * Iterative Lazy Segment Tree (Range Update, Range Query)
+ * Default: Range Add / Range Sum. 
+ * Customize ID, LAZY_ID, comb(), and apply() for other operations.
  */
 template <class T> struct LazySegTree {
     T ID = 0;
     T LAZY_ID = 0;
-    int n;
-    vector<T> tree, lazy;
 
     T comb(T a, T b) { return a + b; }
 
-    LazySegTree(int _n) : n(_n), tree(4 * _n, ID), lazy(4 * _n, LAZY_ID) {}
-    LazySegTree(const vector<T>& v) : n(v.size()), tree(4 * v.size(), ID), lazy(4 * v.size(), LAZY_ID) {
-        build(v, 1, 0, n - 1);
+    void apply(int i, T val) {
+        tree[i] = val * sz[i];
+        if (i < n) lazy[i] = val;
     }
 
-    void push(int node, int tl, int tr) {
-        if (lazy[node] != LAZY_ID) {
-            tree[node] += lazy[node] * (tr - tl + 1);
-            if (tl != tr) {
-                lazy[2 * node] += lazy[node];
-                lazy[2 * node + 1] += lazy[node];
-            }
-            lazy[node] = LAZY_ID;
+    int n, h;
+    vector<T> tree, lazy;
+    vector<int> sz;
+
+    LazySegTree(int _n = 0) {
+        if (_n) init(_n);
+    }
+
+    LazySegTree(const vector<T>& v) {
+        init(v.size());
+        for (int i = 0; i < n; i++) tree[n + i] = v[i];
+        for (int i = n - 1; i > 0; --i) tree[i] = comb(tree[2 * i], tree[2 * i + 1]);
+    }
+
+    void init(int _n) {
+        n = _n;
+        h = 32 - __builtin_clz(n);
+        tree.assign(2 * n, ID);
+        lazy.assign(n, LAZY_ID);
+        sz.assign(2 * n, 0);
+        for (int i = 0; i < n; i++) sz[n + i] = 1;
+        for (int i = n - 1; i > 0; --i) sz[i] = sz[2 * i] + sz[2 * i + 1];
+    }
+
+    void push(int i) {
+        if (lazy[i] != LAZY_ID) {
+            apply(2 * i, lazy[i]);
+            apply(2 * i + 1, lazy[i]);
+            lazy[i] = LAZY_ID;
         }
     }
 
-    void build(const vector<T>& v, int node, int tl, int tr) {
-        if (tl == tr) {
-            tree[node] = v[tl];
-            return;
+    void upd(int l, int r, T val) {
+        l += n; r += n + 1;
+        int l0 = l, r0 = r - 1;
+
+        for (int s = h; s > 0; --s) {
+            int i1 = l0 >> s, i2 = r0 >> s;
+            if (i1 > 0 && i1 < n) push(i1);
+            if (i2 > 0 && i2 < n) push(i2);
         }
-        int tm = (tl + tr) / 2;
-        build(v, 2 * node, tl, tm);
-        build(v, 2 * node + 1, tm + 1, tr);
-        tree[node] = comb(tree[2 * node], tree[2 * node + 1]);
+
+        for (; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) apply(l++, val);
+            if (r & 1) apply(--r, val);
+        }
+
+        for (l0 >>= 1; l0 > 0; l0 >>= 1) tree[l0] = comb(tree[2 * l0], tree[2 * l0 + 1]);
+        for (r0 >>= 1; r0 > 0; r0 >>= 1) tree[r0] = comb(tree[2 * r0], tree[2 * r0 + 1]);
     }
 
-    void upd(int l, int r, T val, int node, int tl, int tr) {
-        push(node, tl, tr);
-        if (l > tr || r < tl) return;
-        if (l <= tl && tr <= r) {
-            lazy[node] += val;
-            push(node, tl, tr);
-            return;
-        }
-        int tm = (tl + tr) / 2;
-        upd(l, r, val, 2 * node, tl, tm);
-        upd(l, r, val, 2 * node + 1, tm + 1, tr);
-        tree[node] = comb(tree[2 * node], tree[2 * node + 1]);
-    }
-    void upd(int l, int r, T val) { upd(l, r, val, 1, 0, n - 1); }
+    T query(int l, int r) {
+        l += n; r += n + 1;
+        int l0 = l, r0 = r - 1;
 
-    T query(int l, int r, int node, int tl, int tr) {
-        push(node, tl, tr);
-        if (l > tr || r < tl) return ID;
-        if (l <= tl && tr <= r) return tree[node];
-        int tm = (tl + tr) / 2;
-        return comb(query(l, r, 2 * node, tl, tm), 
-                    query(l, r, 2 * node + 1, tm + 1, tr));
+        for (int s = h; s > 0; --s) {
+            int i1 = l0 >> s, i2 = r0 >> s;
+            if (i1 > 0 && i1 < n) push(i1);
+            if (i2 > 0 && i2 < n) push(i2);
+        }
+
+        T res = ID;
+        for (; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) res = comb(res, tree[l++]);
+            if (r & 1) res = comb(res, tree[--r]);
+        }
+        return res;
     }
-    T query(int l, int r) { return query(l, r, 1, 0, n - 1); }
 };

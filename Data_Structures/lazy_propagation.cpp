ba0b6b2157b607184276566c41cdc3fdecbf3f8e
wassim <wassim_fkih_pr@proton.me>
2026-05-28 16:06:49 +0100

    reformat lazy_propagation.cpp like segment_tree.cpp

--- Data_Structures/lazy_propagation.cpp
+++ Data_Structures/lazy_propagation.cpp
@@ -1,58 +1,65 @@
-template<typename T>
-struct SegmentTree{
+/**
+ * Lazy Segment Tree (Range Update, Range Query)
+ * Interchangeable with standard SegTree. 
+ * Default configured for Range Add / Range Sum.
+ */
+template <class T> struct LazySegTree {
+    const T ID = 0;
+    const T LAZY_ID = 0;
     int n;
-    vector<T> tree,lazy;
+    vector<T> tree, lazy;
 
-    SegmentTree(int size){
-        n=size;
-        tree.assign(4*n,T());
-        lazy.assign(4*n,T());
+    T comb(T a, T b) { return a + b; }
+
+    LazySegTree(int n) : n(n), tree(4 * n, ID), lazy(4 * n, LAZY_ID) {}
+    LazySegTree(const vector<T>& v) : n(v.size()), tree(4 * v.size(), ID), lazy(4 * v.size(), LAZY_ID) {
+        build(v, 1, 0, n - 1);
     }
 
-    void push(int node,int start,int end){
-        if(lazy[node]!=T()){
-            tree[node]+=lazy[node]*(end-start+1);
-            if(start!=end){
-                lazy[node*2]+=lazy[node];
-                lazy[node*2+1]+=lazy[node];
+    void push(int node, int tl, int tr) {
+        if (lazy[node] != LAZY_ID) {
+            tree[node] += lazy[node] * (tr - tl + 1);
+            if (tl != tr) {
+                lazy[2 * node] += lazy[node];
+                lazy[2 * node + 1] += lazy[node];
             }
-            lazy[node]=T();
+            lazy[node] = LAZY_ID;
         }
     }
 
-    void build(vector<T>&a,int node=1,int start=0,int end=-1){
-        if(end==-1)end=n-1;
-        if(start==end){
-            tree[node]=a[start];
+    void build(const vector<T>& v, int node, int tl, int tr) {
+        if (tl == tr) {
+            tree[node] = v[tl];
             return;
         }
-        int mid=(start+end)/2;
-        build(a,node*2,start,mid);
-        build(a,node*2+1,mid+1,end);
-        tree[node]=tree[node*2]+tree[node*2+1];
+        int tm = (tl + tr) / 2;
+        build(v, 2 * node, tl, tm);
+        build(v, 2 * node + 1, tm + 1, tr);
+        tree[node] = comb(tree[2 * node], tree[2 * node + 1]);
     }
 
-    void update(int l,int r,T val,int node=1,int start=0,int end=-1){
-        if(end==-1)end=n-1;
-        push(node,start,end);
-        if(start>r||end<l)return;
-        if(start>=l&&end<=r){
-            lazy[node]+=val;
-            push(node,start,end);
+    void upd(int l, int r, T val, int node, int tl, int tr) {
+        push(node, tl, tr);
+        if (l > tr || r < tl) return;
+        if (l <= tl && tr <= r) {
+            lazy[node] += val;
+            push(node, tl, tr);
             return;
         }
-        int mid=(start+end)/2;
-        update(l,r,val,node*2,start,mid);
-        update(l,r,val,node*2+1,mid+1,end);
-        tree[node]=tree[node*2]+tree[node*2+1];
+        int tm = (tl + tr) / 2;
+        upd(l, r, val, 2 * node, tl, tm);
+        upd(l, r, val, 2 * node + 1, tm + 1, tr);
+        tree[node] = comb(tree[2 * node], tree[2 * node + 1]);
     }
+    void upd(int l, int r, T val) { upd(l, r, val, 1, 0, n - 1); }
 
-    T query(int l,int r,int node=1,int start=0,int end=-1){
-        if(end==-1)end=n-1;
-        push(node,start,end);
-        if(start>r||end<l)return T();
-        if(start>=l&&end<=r)return tree[node];
-        int mid=(start+end)/2;
-        return query(l,r,node*2,start,mid)+query(l,r,node*2+1,mid+1,end);
+    T query(int l, int r, int node, int tl, int tr) {
+        push(node, tl, tr);
+        if (l > tr || r < tl) return ID;
+        if (l <= tl && tr <= r) return tree[node];
+        int tm = (tl + tr) / 2;
+        return comb(query(l, r, 2 * node, tl, tm), 
+                    query(l, r, 2 * node + 1, tm + 1, tr));
     }
+    T query(int l, int r) { return query(l, r, 1, 0, n - 1); }
 };

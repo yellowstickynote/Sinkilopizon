5161b75c582275a9c977f383adb3b329dffb5afe
wassim <wassim_fkih_pr@proton.me>
2026-05-30 14:14:49 +0100

    update lazy

--- Data_Structures/lazy_propagation.cpp
+++ Data_Structures/lazy_propagation.cpp
@@ -1,21 +1,29 @@
 /**
- * Iterative Lazy Segment Tree (Range Update, Range Query)
- * Default: Range Add / Range Sum. 
- * Customize ID, LAZY_ID, comb(), and apply() for other operations.
+ * Iterative Lazy Segment Tree (Dual-Type Configuration)
+ * * T: Data type of the tree nodes (e.g., struct for matrix, or node sums)
+ * * U: Data type of the lazy tags (e.g., struct for affine transformation mx+b)
+ * Default: T and U are the same type. To separate, instantiate as LazySegTree<T, U>.
+ * Only edit the Customization Section at the top.
  */
-template <class T> struct LazySegTree {
+template <class T, class U = T> struct LazySegTree {
     T ID = 0;
-    T LAZY_ID = 0;
+    U LAZY_ID = 0;
 
-    T comb(T a, T b) { return a + b; }
+    T comb(T a, T b) { 
+        return a + b; 
+    }
 
-    void apply(int i, T val) {
-        tree[i] += val * sz[i];
-        if (i < n) lazy[i] += val;
+    T apply_val(T node_val, U lazy_val, int node_sz) { 
+        return node_val + lazy_val * node_sz; 
+    }
+
+    U combine_lazy(U old_lazy, U new_lazy) { 
+        return old_lazy + new_lazy; 
     }
 
     int n, h;
-    vector<T> tree, lazy;
+    vector<T> tree;
+    vector<U> lazy;
     vector<int> sz;
 
     LazySegTree(int _n = 0) {
@@ -38,6 +46,11 @@ template <class T> struct LazySegTree {
         for (int i = n - 1; i > 0; --i) sz[i] = sz[2 * i] + sz[2 * i + 1];
     }
 
+    void apply(int i, U val) {
+        tree[i] = apply_val(tree[i], val, sz[i]);
+        if (i < n) lazy[i] = combine_lazy(lazy[i], val);
+    }
+
     void push(int i) {
         if (lazy[i] != LAZY_ID) {
             apply(2 * i, lazy[i]);
@@ -46,7 +59,12 @@ template <class T> struct LazySegTree {
         }
     }
 
-    void upd(int l, int r, T val) {
+    void calc(int i) {
+        tree[i] = comb(tree[2 * i], tree[2 * i + 1]);
+        if (lazy[i] != LAZY_ID) tree[i] = apply_val(tree[i], lazy[i], sz[i]);
+    }
+
+    void upd(int l, int r, U val) {
         l += n; r += n + 1;
         int l0 = l, r0 = r - 1;
 
@@ -61,14 +79,8 @@ template <class T> struct LazySegTree {
             if (r & 1) apply(--r, val);
         }
 
-        for (l0 >>= 1; l0 > 0; l0 >>= 1) {
-            tree[l0] = comb(tree[2 * l0], tree[2 * l0 + 1]);
-            if (lazy[l0] != LAZY_ID) tree[l0] += lazy[l0] * sz[l0];
-        }
-        for (r0 >>= 1; r0 > 0; r0 >>= 1) {
-            tree[r0] = comb(tree[2 * r0], tree[2 * r0 + 1]);
-            if (lazy[r0] != LAZY_ID) tree[r0] += lazy[r0] * sz[r0];
-        }
+        for (l0 >>= 1; l0 > 0; l0 >>= 1) calc(l0);
+        for (r0 >>= 1; r0 > 0; r0 >>= 1) calc(r0);
     }
 
     T query(int l, int r) {
